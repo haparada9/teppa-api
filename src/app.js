a711989e7972a15f4b38222fab3dc590fa8b0c39
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-07-28 23:36:51 -0300

    feat: :sparkles: Cria a rota PUT

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const candidatos = [
   { id: 1, nome: "Hugo Parada" },
@@ -17,12 +17,19 @@ app.get("/candidatos", (req, res) => {
   res.status(200).json(candidatos);
 });
 
-app.post('/candidatos', (req, res) => {
-    candidatos.push(req.body)
-    res.status(201).send('Candidato cadastrado com sucesso!')
-})
+app.post("/candidatos", (req, res) => {
+  candidatos.push(req.body);
+  res.status(201).send("Candidato cadastrado com sucesso!");
+});
 
+app.put("/candidatos/:id", (req, res) => {
+  let index = buscaCandidato(req.params.id);
+  candidatos[index].nome = req.body.nome;
+  res.json(candidatos);
+});
 
-
+function buscaCandidato(id) {
+  return candidatos.findIndex((candidato) => candidato.id == id);
+}
 
 export default app;
